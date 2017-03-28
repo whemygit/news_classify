f2811bcb4b28ebc67e1f23aaeadfef5880d72e1d
whemygit <whemy@sina.com>
2017-03-28 22:32:29 +0800

    0328-n

--- SVD/machine_learing.py
+++ SVD/machine_learing.py
@@ -6,12 +6,12 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 if __name__ == '__main__':
     pass
-
+#####################################################################################
 from numpy import *
 U,Sigma,T=linalg.svd([[1,1],[7,7]])
 # print U,Sigma,T
 
-
+#矩阵分解
 def loadExData():
     return [[1,1,1,0,0],
             [2,2,2,0,0],
@@ -23,10 +23,37 @@ def loadExData():
 
 Data=loadExData()
 U,Sigma,VT=linalg.svd(Data)
-print Sigma
+# print Sigma
+#
+# Sig3=mat([[Sigma[0],0,0],[0,Sigma[1],0],[0,0,Sigma[2]]])
+# print Sig3
+# print Sig3[0:2,:]
+#
+# print U[:,:3]*Sig3*VT[:3,:]
 
-Sig3=mat([[Sigma[0],0,0],[0,Sigma[1],0],[0,0,Sigma[2]]])
-print Sig3
-print Sig3[0:2,:]
 
-print U[:,:3]*Sig3*VT[:3,:]
+##############################################################################
+#相似度计算
+from numpy import *
+from numpy import linalg as la
+
+def ecludSim(inA,inB):                                 #欧氏距离相似性
+    return 1.0/(1.0+la.norm(inA-inB))
+
+def pearsSim(inA,inB):                                  #皮尔逊相关相似性
+    if len(inA)<3:return 1.0
+    return 0.5+0.5*corrcoef(inA,inB,rowvar=0)[0][1]
+
+def cosSim(inA,inB):                                    #余弦相似性
+    num=float(inA.T*inB)
+    denom=la.norm(inA)*la.norm(inB)
+    return 0.5+0.5*(num/denom)
+
+# myMat=mat(loadExData())
+# print ecludSim(myMat[:,0],myMat[:,4])
+# print ecludSim(myMat[:,0],myMat[:,0])
+# print cosSim(myMat[:,0],myMat[:,4])
+# print cosSim(myMat[:,0],myMat[:,0])
+# print pearsSim(myMat[:,0],myMat[:,4])
+# print pearsSim(myMat[:,0],myMat[:,0])
+
