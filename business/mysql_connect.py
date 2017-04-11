7700a9e02f8160da6ac85aef681912d9dcc6e030
whemygit <whemy@sina.com>
2017-04-11 18:55:43 +0800

    商机分类

---
+++ business/mysql_connect.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -- coding: utf-8 --
+import sys
+import torndb
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+#连接数据库
+def mysql_connect():
+    mysql_par={'ip':"192.168.0.202",
+               'port':'3306',
+               'database':'spider',
+               'user':'root',
+               'password':'neiwang-zhongguangzhangshi'}
+
+    db=torndb.Connection(host=mysql_par['ip'],
+                         database=mysql_par['database'],
+                         user=mysql_par['user'],
+                         password=mysql_par['password'])
+    return db
+
